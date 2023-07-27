b522650e0ca5b79cb08e0ac2c26a7d8f512cec21
PhamSang1210 <phamhongsang12x10@gmail.com>
2023-07-27 11:15:11 +0700

    RAW

--- css/main.js
+++ css/main.js
@@ -1,2 +1,14 @@
-let root = document.getElementById("root");
-console.log(root);
+// Lấy đối tượng HTML chứa văn bản
+const element = document.getElementById("root");
+
+// Lấy nội dung văn bản
+const text = element.innerText;
+
+// Tạo đối tượng Blob từ văn bản
+const blob = new Blob([text], { type: "text/plain" });
+
+// Tạo URL từ đối tượng Blob
+const url = URL.createObjectURL(blob);
+
+// Mở trang web mới với đối tượng Blob
+window.open(url);
